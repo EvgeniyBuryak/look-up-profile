013926ab4dafea5fff603a56243d5f2fcafce8f7
Jhonah <buryakone@gmail.com>
2021-06-09 19:01:44 +0700

    init commit

---
+++ script.js
@@ -0,0 +1,53 @@
+// Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+// Поиск профиля
+function lookUpProfile(name, prop) {
+    let result = "";
+    for (let key in contacts) {
+        let obj = contacts[key];
+        let isOwnProperty = obj.hasOwnProperty(prop);
+        if (obj.firstName == name) {
+            if (isOwnProperty) {
+                result = obj[prop];
+                break;
+            } else {
+                result = "No such property";
+                break;
+            }
+        } else {
+            result = "No such contact";
+        }
+    }
+    return result;
+}
+
+let result = lookUpProfile("Akira", "address");
+console.log(result);
+result = lookUpProfile("Kristian", "lastName");
+console.log(result);
